f6fb947d22b35ee554c65ad29d18496de1320da6
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-01 18:42:47 +0900

    fizzbuzz復習

--- array2.rb
+++ array2.rb
@@ -47,3 +47,22 @@ def fizzbuzz(i)
 end
 
 fizzbuzz(gets.to_i)
+
+def fizz_buzz(number)
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number.to_s
+  end
+end
+
+puts "数字を入力してください。"
+
+input = gets.to_i
+
+puts "結果は..."
+puts fizz_buzz(input)
