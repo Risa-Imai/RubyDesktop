8fe0b9846585b9339ce1580e4c6b581e28a32f84
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-11 01:20:55 +0900

    {Add]Document.rb

--- Document.rb
+++ Document.rb
@@ -9,3 +9,25 @@ def hihi(name = "World")
 end
 
 p hihi
+
+class Greeter
+  def initialize(name = "World")
+    @name = name
+  end
+
+  def say_hi
+    puts "Hi #{@name}!"
+  end
+
+  def say_bye
+    puts "Bye #{@name}, come back soon."
+  end
+end
+
+greeter = Greeter.new("Pat")
+p greeter
+puts greeter
+print greeter
+
+p greeter.say_hi
+p greeter.say_bye
