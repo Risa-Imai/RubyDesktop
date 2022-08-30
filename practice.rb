02177343f0d95fef238a4c1632c5cf2a3be644a0
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-30 21:09:36 +0900

    add

--- practice.rb
+++ practice.rb
@@ -1,20 +1,18 @@
 def fizz_buzz(num)
-  if (num.to_i % 3 == 0) && (num.to_i % 5 == 0)
+  if num.is_a?(String)
+    puts "入力された文字は#{num}"
+  elsif (num.to_i % 3 == 0) && (num.to_i % 5 == 0)
     puts "FizzBuzz"
   elsif num.to_i % 5 == 0
     puts "Fizz"
   elsif num.to_i % 3 == 0
     puts "buzz"
   else
-    puts "入力された数字は#{num}"
+    puts "入力された文字は#{num}"
   end
 end
 
 puts "数字を入力してください"
 input = gets
-until input =~ /^[0-9]+$/
-  puts "正しい数字を入力してください"
-  input = gets
-end
 
 fizz_buzz(input)
