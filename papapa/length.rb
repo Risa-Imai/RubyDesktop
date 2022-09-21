42b02f7c6742c87eded9e7a9b65c484ee2bfd6b9
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-21 10:48:14 +0900

    add

--- papapa/length.rb
+++ papapa/length.rb
@@ -2,4 +2,8 @@ line = readlines.map(&:chomp)
 li = line.max { |a, b| a.length <=> b.length }
 puts li.length
 
-input = gets.chomp!.split(" ").map { |n| n.to_i }
+# input = gets.chomp.split(" ").map { |n| n.to_i }
+
+fruits = { "apple" => 100, "orange" => 200, "grape" => 300 }
+kudamono = fruits.map { |key, value| [key, value * 2] }
+p kudamono.to_h
