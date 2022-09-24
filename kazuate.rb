b136ff36fa0d51e8b42d3cfcfc41e485795c54ba
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-24 19:46:23 +0900

    途中

--- kazuate.rb
+++ kazuate.rb
@@ -1,3 +1,11 @@
 random = Random.new
 i = random.rand(1..15)
-puts i
+puts "数当てゲーム"
+puts "1~15の間の数字がランダムで生成されます"
+print "生成された数字を当ててください＝＞"
+num = gets.to_i
+if i == num
+  puts "正解！"
+else
+  puts "はずれ！"
+end
