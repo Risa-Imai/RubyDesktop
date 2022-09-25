f848d7b13b773005ba934b8677694a9424a4babe
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-26 07:58:35 +0900

    fix kazuate

--- kazuate.rb
+++ kazuate.rb
@@ -1,18 +1,20 @@
 random = Random.new
 i = random.rand(1..15)
-puts i
+ans = []
 puts "数当てゲーム"
 puts "1~15の間の数字がランダムで生成されます"
 puts "10回で当ててください"
 10.downto(1) do |x|
   puts "残り#{x}回"
+  puts "入力済みの数字#{ans}" if ans.length > 0
   print "生成された数字を当ててください＝＞"
   num = gets.to_i
+  ans << num
   if i == num
     puts "正解！"
     break
   else
-    puts "はずれ！"
+    print "はずれ！"
     if num > i
       puts "大きすぎます！"
     else
@@ -20,3 +22,4 @@ puts "10回で当ててください"
     end
   end
 end
+puts "時間切れです！答えは#{i}でした" unless ans.include?(i)
