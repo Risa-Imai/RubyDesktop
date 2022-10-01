e74bf477561327b08a2c962c23b1fbc3f914709b
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-01 21:47:28 +0900

    fix

--- papapa/D165.rb
+++ papapa/D165.rb
@@ -1,11 +1,11 @@
 s = gets.split("").map &:to_i
 result = []
 9.downto(1) do |i|
-    result << s.count(i)
+  result << s.count(i)
 end
 result.map(&:to_i)
-if result.include?(2) || result.include?(3)
-    puts "NG"
+if result.include?(2) || result.include?(3) || result.include?(4)
+  puts "NG"
 else
-    puts "OK"
+  puts "OK"
 end
